f8dd634676b5c80abbed0b9a32ce9f3e632cbb6f
zhangmh <zhangmh@ucweb.com>
2013-09-29 16:45:40 +0800

    搭建项目结构

---
+++ src/js/base/display.js
@@ -0,0 +1,25 @@
+/**
+ * 显示类
+ */
+var Display = function Display () {
+    // body...
+};
+Display.prototype.init = function(option) {
+    // body...
+};
+
+Display.prototype.render = function() {
+    // body...
+};
+
+Display.prototype.show = function() {
+    // body...
+};
+
+Display.prototype.hide = function() {
+    // body...
+};
+
+Display.prototype.destroy = function() {
+    // body...
+};
