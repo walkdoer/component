d0b4174dd589d1abefadf7bd552ef35be64ddcef
zhangmh <zhangmh@ucweb.com>
2013-10-02 17:31:59 +0800

    新建基类display

--- src/js/base/display.js
+++ src/js/base/display.js
@@ -4,8 +4,22 @@
 define(function (require, exports) {
     'use strict';
     var Class = require('lib/class'),
+        setVisibility,
         Display;
-
+    /**
+     * 设置元素显示还是隐藏
+     * @param {Object}  element [Dom元素]
+     * @param {Boolean} isShow  [true:显示, false: 隐藏]
+     */
+    setVisibility = function setVisibility(element, isShow) {
+        var func = isShow ? 'show' : 'none',
+            style = isShow ? 'block' : 'none';
+        if (element[func] === 'function') {
+            element.show();
+        } else {
+            element.style.display = style;
+        }
+    };
     Display = Class.extend({
         /**
          * 初始化函数
@@ -21,13 +35,13 @@ define(function (require, exports) {
          * 显示
          */
         show: function show() {
-
+            setVisibility(this.container, true);
         },
         /**
          * 隐藏
          */
         hide: function hide() {
-
+            setVisibility(this.container, false);
         },
         /**
          * 渲染
