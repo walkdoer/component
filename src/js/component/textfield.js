458260ec4b3d71a7617035c984a47e4fe7e7809a
zhangmh <zhangmhao@gmail.com>
2013-11-11 17:42:29 +0800

    删除page中间层，修改textfield组件

--- src/js/component/textfield.js
+++ src/js/component/textfield.js
@@ -24,13 +24,7 @@ define(function (require, exports) {
             },
             //input事件触发比较频繁，需要添加事件防抖
             'input input': _.debounce(function (event) {
-                //由于时间关系，这部分代码先和HTML结构耦合，下一个版本解决耦合问题
-                //耦合处：$field.parent().find('.clear');
-                var $field = $(event.target),
-                    value = $field.val(),
-                    $iconClear = $field.parent().find('.clear');
-                this.value = value;
-                $iconClear[value ? 'show' : 'hide']();
+                this.value = event.target.value;
                 this.trigger('INPUT', [this]);
             }, 300),
             'blur input': function () {
