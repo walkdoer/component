782825baa26c28d3d68c1bfbf4cc4c760b6c02ca
zhangmh <zhangmhao@gmail.com>
2014-04-03 17:21:19 +0800

    增加Com模块单元测试用例

--- test/comTest.js
+++ test/comTest.js
@@ -8,12 +8,33 @@ define(function (require) {
 
     QUnit.test("com Api test", function () {
 
-        var App = new Com({
+        var app = new Com({
             id: 'application',
+            tplContent: '<div><p class="title">test</p></div>',
             parentEl: document.body
         });
 
-        App.render().appendToParent();
+        app.render();
+        QUnit.equal(app.el.innerHTML, '<p class="title">test</p>', "元素HTML正常");
+        app.appendToParent();
+        QUnit.equal(document.getElementById('application'),
+            app.el,
+            'API appendToParent() 正常');
+
+        var topBar = new Com({
+            id: 'topbar',
+            tplContent: '<nav class="<%_state_.name%>"><%title%></nav>',
+            getState: function () {
+                return {
+                    name: 'name'
+                };
+            }
+        });
+
+        app.appendChild(topBar);
+        QUnit.equal(topBar.el.id, topBar.id, 'ID属性正常');
+        QUnit.equal(topBar.el.className, 'name', 'API getState()正常');
+        app.render();
     });
 
 });
